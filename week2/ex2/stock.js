81bba22e27f412ec5a66c4f651a9b3a40f89689b
adam driggers <awdriggs@gmail.com>
2016-09-29 23:47:02 -0400

    in class code

--- week2/ex2/stock.js
+++ week2/ex2/stock.js
@@ -1,1 +1,11 @@
 console.log('working!');
+
+var stockprice = 10; 
+
+while(stockprice >= 10) {
+	stockprice = parseInt(prompt("what is the price?"));
+    
+    alert("current price is $" + stockprice); 
+} 
+
+alert("You should be selling!")
