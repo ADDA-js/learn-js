97ea2a27951826590142c64a32b15db435f2584a
adam driggers <awdriggs@gmail.com>
2018-03-04 16:07:40 -0500

    small edits to w6

--- week6-games/memory_starter/js/app.js
+++ week6-games/memory_starter/js/app.js
@@ -1,10 +1,8 @@
 console.log('working yo')
 
-// USE THIS TO SHUFFLE YOUR NUMBERS
-//+ Jonas Raoni Soares Silva
-//@ http://jsfromhell.com/array/shuffle [v1.0]
+// USE THIS TO SHUFFLE YOUR LETTERS 
+// Code by Jonas Raoni Soares Silva @ http://jsfromhell.com/array/shuffle [v1.0]
 function shuffle(o){ //v1.0
-	//debugger;
 	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 		return o;
 };
