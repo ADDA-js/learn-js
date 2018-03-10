9566e738b55c7e35784342bb0d7cbacdb0ec0238
adam driggers <awdriggs@gmail.com>
2018-03-10 16:07:59 -0500

    update memory game

--- week6-games/memory_starter/js/app.js
+++ week6-games/memory_starter/js/app.js
@@ -2,8 +2,32 @@ console.log('working yo')
 
 // USE THIS TO SHUFFLE YOUR LETTERS 
 // Code by Jonas Raoni Soares Silva @ http://jsfromhell.com/array/shuffle [v1.0]
-function shuffle(o){ //v1.0
-	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-		return o;
+function shuffle(o) { //v1.0
+  for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
 };
 
+var memoryGame = {
+  tiles: ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J"],
+  start: function() {
+    //write code here
+  },
+  makeAndDisplayTiles: function() {
+    //write code here
+  },
+  addEventsToTiles: function() {
+    //write code here
+  },
+  makePlay: function() {
+    //write code here
+  },
+  checkForMatch: function() {
+    //write code here
+  },
+  checkForWin: function() {
+    //write code here
+  }
+}
+
+
+//add document ready function
