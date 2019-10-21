8566ecf0c532eb98c033616d0b7ca3d79a95c15f
adam driggers <awdriggs@gmail.com>
2019-10-20 21:24:37 -0400

    more w5

--- week5-dom/dom/spoofify/app.js
+++ week5-dom/dom/spoofify/app.js
@@ -1,3 +1,16 @@
-window.onload = function(){
-  alert('ready');
+window.onload = function() {
+  console.log('ready');
+  //PART1
+  // Use js to change the change your name in the title
+
+  // Use js to add three songs to the ul with a class of `playlist`
+
+  // Add the class 'now-playing' to the first song you added
+
+  //PART2 
+  // Return the the playlist exercise.
+  // Use “for...of” to make the songs you added previously have a class of 'played'.
+
+  // Use a for loop to add 5 more songs. You can make the names generic, like “new song 1”
+
 }
